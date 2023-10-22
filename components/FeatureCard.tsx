eb3856704d5f1dcde0128b348763e7b01cc8e697
DarKS4ys <melihyardim1057@gmail.com>
2023-10-22 12:09:43 +0300

    small hover update

--- components/FeatureCard.tsx
+++ components/FeatureCard.tsx
@@ -11,11 +11,11 @@ interface CardProps {
 
 export default function FeatureCard({icon, title, color, border, text}: CardProps) {
   return (
-    <div className={clsx('border mr-2 flex flex-col justify-center items-center gap-1 px-16 py-6 rounded-xl bg-opacity-5 hover:bg-opacity-30 transition duration-300', color, border)}>
-        <div className={clsx('rounded-full flex flex-col justify-center items-center p-4 w-14 h-14 bg-opacity-10', color, text)}>
+    <div className={clsx('border mr-2 flex flex-col justify-center group items-center gap-1 px-16 py-6 rounded-xl bg-opacity-5 hover:bg-opacity-30 transition duration-300', color, border)}>
+        <div className={clsx('group-hover:scale-110 rounded-full flex flex-col justify-center items-center p-4 w-14 h-14 bg-opacity-10 group-hover:bg-opacity-30 transition duration-300', color, text)}>
           {icon}
         </div>
-        <p className="text-lg pt-2">{title}</p>
+        <p className="text-lg pt-2 group-hover:scale-110 transition duration-300">{title}</p>
     </div>
   )
 }
